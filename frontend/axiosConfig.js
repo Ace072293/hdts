05a2020eeda65cfa6f5cdaa331907c3d49781b20
Ace072293 <dostacedy@gmail.com>
2025-08-14 15:15:29 +1000

    localhost ip address update

--- frontend/axiosConfig.js
+++ frontend/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000', // Replace with your API base URL
+  baseURL: 'http://13.239.242.22:5001', // Replace with your API base URL
   headers: {
     'Content-Type': 'application/json',
   },
